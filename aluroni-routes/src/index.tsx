ccc38043a2c80d465f2299c67ea9d5d8f40b635a
Fernando Goia <fnagoya_hr2@hotmail.com>
2022-06-29 18:02:37 -0300

    Route

--- aluroni-routes/src/index.tsx
+++ aluroni-routes/src/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'normalize.css';
-import './index.css';
-import Cardapio from './pages/Cardapio';
+import "normalize.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Router from "./routes";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-    <React.StrictMode>
-        <Cardapio />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Router />
+  </React.StrictMode>
 );
