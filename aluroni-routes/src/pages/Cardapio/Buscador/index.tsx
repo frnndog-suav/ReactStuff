aed432240e60df7c59a6738f6eb003c4b8b1dab3
Fernando Goia <fnagoya_hr2@hotmail.com>
2022-06-29 11:02:13 -0300

    Initializing new tutorial project

---
+++ aluroni-routes/src/pages/Cardapio/Buscador/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './Buscador.module.scss';
+import { CgSearch } from 'react-icons/cg';
+
+interface Props {
+  busca: string;
+  setBusca: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Buscador(props: Props) {
+    return (
+        <div className={styles.buscador}>
+            <input
+                value={props.busca}
+                onChange={(evento) => props.setBusca(evento.target.value)}
+                placeholder="Buscar"
+            />
+            <CgSearch size={20} color="#4c4d5f" />
+        </div>
+    );
+}
