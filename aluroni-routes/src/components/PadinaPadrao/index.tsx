f03f789252ad34f793d84cf6ccfae0f48fe71c72
Fernando Goia <fnagoya_hr2@hotmail.com>
2022-06-30 11:52:24 -0300

    Navigate

---
+++ aluroni-routes/src/components/PadinaPadrao/index.tsx
@@ -0,0 +1,17 @@
+import styles from "./PaginaPadrao.module.scss";
+import stylesTema from "styles/Tema.module.scss";
+
+import { Outlet } from "react-router-dom";
+
+export default function PaginaPadrao() {
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.header__text}>A casa do código da massa</div>
+      </header>
+      <div className={stylesTema.titulo}>
+        <Outlet />
+      </div>
+    </>
+  );
+}
--- aluroni-routes/src/pages/Inicio/index.tsx
+++ aluroni-routes/src/pages/Inicio/index.tsx
@@ -1,5 +1,7 @@
 import cardapio from "data/cardapio.json";
 import styles from "./Inicio.module.scss";
+import stylesTema from "styles/Tema.module.scss";
+import nossaCasa from "assets/nossa_casa.png";
 
 export default function Inicio() {
   let pratosRecomendados = [...cardapio];
@@ -9,7 +11,7 @@ export default function Inicio() {
 
   return (
     <section>
-      <h3 className={styles.titulo}>Recomendações da cozinha</h3>
+      <h3 className={stylesTema.titulo}>Recomendações da cozinha</h3>
       <div className={styles.recomendados}>
         {pratosRecomendados.map((item) => (
           <div className={styles.recomendado} key={item.id}>
@@ -19,6 +21,13 @@ export default function Inicio() {
             <button className={styles.recomendado__botao}>Ver mais</button>
           </div>
         ))}
+      </div>
+      <h3 className={stylesTema.titulo}>Nossa Casa</h3>
+      <div className={styles.nossaCasa}>
+        <img src={nossaCasa} alt="Casa do Aluroni" />
+        <div className={styles.nossaCasa__endereco}>
+          Rua Vergueiro,1234 <br></br> Vila Mariana - SP
+        </div>
       </div>
     </section>
   );
