f88672e0908011a0183a896857fa943e40fa64ad
Fernando Goia <fnagoya_hr2@hotmail.com>
2022-06-30 21:08:30 -0300

    Navigate (state)

--- aluroni-routes/src/pages/Prato/index.tsx
+++ aluroni-routes/src/pages/Prato/index.tsx
@@ -1,8 +1,7 @@
-import styles from "./Prato.module.scss";
-import { useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export default function Prato() {
-  const test = useParams();
+  console.log(useLocation());
 
   return <div>Prato</div>;
 }
