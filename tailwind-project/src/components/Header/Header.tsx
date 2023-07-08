65ccde06dec2672568e322f8c31ccfead4c16b04
Fernando Goia <f97.goya@gmail.com>
2023-07-08 18:02:28 -0300

    Tailwind configuration file (theme colors)

--- tailwind-project/src/components/Header/Header.tsx
+++ tailwind-project/src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import ToggleTheme from "../ToggleTheme/ToggleTheme";
 
 const Header: FC = () => {
   return (
-    <div className="flex h-20 bg-gray-500 justify-between items-center px-20 sm:rounded-xl sm:m-5">
+    <div className="flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-20 sm:rounded-xl sm:m-5">
       <span className="text-gray-100">Ola usuário</span>
       <h1 className="text-gray-100 text-xl">Meu projeto exemplo</h1>
       <ToggleTheme />
