ead70a78fe5cced58e8e1dcde3082f8d40b1de41
Fernando Goia <f97.goya@gmail.com>
2023-08-03 19:02:38 -0300

    Data-test selector

--- testes-e2e-cypress-main/cypress/e2e/inicio.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/inicio.cy.js
@@ -1,7 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Página inicial', () => {
+  it('Deve renderizar o h1 com o texto correto', () => {
     cy.visit('http://localhost:3000');
-    cy.get('h1').contains(
+    cy.get('[data-test="titulo-principal"]').contains(
       'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
     );
   });
