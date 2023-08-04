dd88e589f8c7db88aa2b85a492b5648d47314424
Fernando Goia <f97.goya@gmail.com>
2023-08-03 21:15:04 -0300

    More tests

---
+++ testes-e2e-cypress-main/cypress/e2e/formularioLogin.cy.js
@@ -0,0 +1,15 @@
+describe('Formulário de login', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Não deve permitir um email inválido', () => {
+    cy.getByData('botao-login').click();
+    cy.getByData('email-input').type('neilton@alura');
+    cy.getByData('senha-input').type('123456');
+    cy.getByData('botao-enviar').click();
+    cy.getByData('mensagem-erro')
+      .should('exist')
+      .and('have.text', 'O email digitado é inválido');
+  });
+});
