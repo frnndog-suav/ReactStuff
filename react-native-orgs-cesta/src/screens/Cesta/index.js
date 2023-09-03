00a44f751537c8ea87307e0500ca138a1ffafbba
Fernando Goia <f97.goya@gmail.com>
2023-09-03 15:29:47 -0300

    Using assets

--- react-native-orgs-cesta/src/screens/Cesta/index.js
+++ react-native-orgs-cesta/src/screens/Cesta/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Image, StyleSheet, Dimensions, Text } from "react-native";
+import { Image, StyleSheet, Dimensions, Text, View } from "react-native";
 import topoImagePath from "../../../assets/topo.png";
+import farmLogo from "../../../assets/logo.png";
 
 const width = Dimensions.get("screen").width;
 
@@ -17,7 +18,39 @@ const estilos = StyleSheet.create({
     color: "white",
     fontWeight: "bold",
     padding: 16,
-    marginTop: 50
+    marginTop: 50,
+  },
+  nome: {
+    color: "#464646",
+    fontSize: 26,
+    fontWeight: "bold",
+  },
+  cesta: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  nomeFazenda: {
+    fontSize: 16,
+    marginLeft: 12,
+  },
+  descricao: {
+    color: "#A3a3a3",
+    fontSize: 16,
+  },
+  preco: {
+    color: "#2a9f85",
+    fontWeight: "bold",
+    fontSize: 26,
+    marginTop: 8,
+  },
+  imgFazenda: {
+    width: 32,
+    height: 32,
+  },
+  fazenda: {
+    flexDirection: "row",
+    paddingVertical: 12,
+    alignItems: 'center'
   },
 });
 
@@ -26,6 +59,19 @@ const Cesta = () => {
     <React.Fragment>
       <Image style={estilos.topo} source={topoImagePath} />
       <Text style={estilos.titulo}>Detalhe da cesta</Text>
+      <View style={estilos.cesta}>
+        <Text style={estilos.nome}>Cesta de verduras</Text>
+        <View style={estilos.fazenda}>
+          <Image style={estilos.imgFazenda} source={farmLogo} />
+          <Text style={estilos.nomeFazenda}>Jenny and Jack farms</Text>
+        </View>
+
+        <Text style={estilos.descricao}>
+          Uma cesta com produtos selecionados cuidadosamente da fazenda para sua
+          cozinha
+        </Text>
+        <Text style={estilos.preco}>R$ 40,00</Text>
+      </View>
     </React.Fragment>
   );
 };
