700e088b488295481bc5a58fb2d785b8d7eee719
Fernando Goia <f97.goya@gmail.com>
2023-11-04 12:03:05 -0300

    Displaying mocked data

--- ignite-react-basics/src/App.jsx
+++ ignite-react-basics/src/App.jsx
@@ -4,6 +4,89 @@ import { Post } from "./components/Post/Post";
 import { Sidebar } from "./components/Sidebar/Sidebar";
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl:
+        "https://images.pexels.com/photos/4814640/pexels-photo-4814640.jpeg?auto=compress&cs=tinysrgb&w=200&h=750&dpr=1",
+      name: "Michelle",
+      role: "Modelo",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Integer vitae dolor id turpis dictum lobortis.",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Vivamus quis justo luctus, laoreet arcu malesuada, vehicula diam.",
+      },
+      {
+        type: "paragraph",
+        content: "Nam mattis ex et nunc dictum, non tincidunt metus molestie.",
+      },
+      { type: "link", content: "Fusce" },
+    ],
+    publishedAt: new Date("2023-11-04"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl:
+        "https://images.pexels.com/photos/3487734/pexels-photo-3487734.jpeg?auto=compress&cs=tinysrgb&w=200&h=750&dpr=1",
+      name: "Jupiter",
+      role: "Cachorro",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content:
+          "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+      },
+      {
+        type: "paragraph",
+        content: "Quisque ac eros blandit sem faucibus congue.",
+      },
+      {
+        type: "paragraph",
+        content: "Nunc in porttitor nunc, mollis vestibulum erat.",
+      },
+      {
+        type: "paragraph",
+        content: "Nunc non lacus venenatis, congue libero non, tincidunt odio.",
+      },
+      { type: "paragraph", content: "Cras dignissim pharetra enim ac rutrum." },
+      { type: "link", content: "Suspendisse" },
+    ],
+    publishedAt: new Date("2023-11-03"),
+  },
+  {
+    id: 3,
+    author: {
+      avatarUrl:
+        "https://images.pexels.com/photos/3057984/pexels-photo-3057984.jpeg?auto=compress&cs=tinysrgb&w=300&h=750&dpr=1",
+      name: "Eduardo",
+      role: "Dono de loja",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content:
+          "In volutpat, turpis quis pellentesque vulputate, turpis enim tincidunt felis, sed sollicitudin nibh ex ac mi.",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Pellentesque sed turpis et ipsum pellentesque posuere in ac erat.",
+      },
+      { type: "link", content: "Quisque" },
+    ],
+    publishedAt: new Date("2023-11-02"),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -11,7 +94,10 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          {/* <Post /> */}
+          {posts.map((post) => (
+            <Post key={post.id} {...post} />
+          ))}
         </main>
       </div>
     </div>
--- ignite-react-basics/src/components/Post/Post.jsx
+++ ignite-react-basics/src/components/Post/Post.jsx
@@ -1,33 +1,51 @@
+/* eslint-disable react/prop-types */
+import { format, formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import { Avatar } from "../Avatar/Avatar";
 import { Comment } from "../Comment/Comment";
 import styles from "./Post.module.css";
 
-export function Post() {
+export function Post({ id, content, author, publishedAt }) {
+  const formattedPublishedAt = format(publishedAt, "d 'de' LLLL 'às' HH'h'mm", {
+    locale: ptBR,
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://images.unsplash.com/photo-1508179522353-11ba468c4a1c?auto=format&fit=crop&q=30&w=200&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Teste Silva</strong>
-            <span>Programador</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="2 de Novembro de 2023" dateTime="2023-11-02">
-          Publicado há 1h
+        <time title={formattedPublishedAt} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
         </time>
       </header>
       <div className={styles.content}>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <p>Morbi id tellus lobortis, viverra dolor placerat, hendrerit nisi.</p>
-        <p>In eu ante vel nisl varius tempor. In sed congue orci.</p>
-        <p>Nulla facilisis nibh a vestibulum scelerisque.</p>
-        <p>
-          <a href="#">#teste</a>
-          <a href="#">#projectoBase</a>
-          <a href="#">#rockeseat</a>
-        </p>
+        {content.map((item) => {
+          if (item.type === "paragraph") {
+            return (
+              <p key={`${id}-${item.type}-${item.content}`}>{item.content}</p>
+            );
+          }
+
+          if (item.type === "link") {
+            return (
+              <p key={`${id}-${item.type}-${item.content}`}>
+                <a href="">{item.content}</a>
+              </p>
+            );
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
