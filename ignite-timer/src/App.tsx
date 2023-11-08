ead71f1feaf9199f0704e6fc631a8c574aa89dd4
Fernando Goia <f97.goya@gmail.com>
2023-11-08 10:23:18 -0300

    Using styled components theme

--- ignite-timer/src/App.tsx
+++ ignite-timer/src/App.tsx
@@ -1,12 +1,14 @@
+import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/theme/default";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="danger" />
       <Button variant="secondary" />
       <Button variant="primary" />
       <Button variant="success" />
-    </>
+    </ThemeProvider>
   );
 }
