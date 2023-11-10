4c82d48f48a84ec3c631575ff8b407be2c3713a4
Fernando Goia <f97.goya@gmail.com>
2023-11-10 14:51:30 -0300

    Implementing countdown display

--- ignite-timer/src/pages/Home/index.tsx
+++ ignite-timer/src/pages/Home/index.tsx
@@ -32,6 +32,7 @@ interface Cycle {
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([]);
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
 
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(formSchema),
@@ -44,6 +45,15 @@ export function Home() {
   const taskInputValue = watch("task");
   const isSubmitDisabled = !taskInputValue;
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
+
+  const minutes = String(minutesAmount).padStart(2, "0");
+  const seconds = String(secondsAmount).padStart(2, "0");
 
   function handleTimerForm(data: NewCycleFormData) {
     const newCycle: Cycle = {
@@ -91,11 +101,11 @@ export function Home() {
         </FormContainer>
 
         <CountdownContainer>
-          <span>{"0"}</span>
-          <span>{"0"}</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
-          <span>{"0"}</span>
-          <span>{"0"}</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </CountdownContainer>
 
         <StartCountdownButton type="submit" disabled={isSubmitDisabled}>
