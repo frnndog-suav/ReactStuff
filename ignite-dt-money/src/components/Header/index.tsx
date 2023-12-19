b5a4f8f7185e838afb611ab8bd2474cab78287b7
Fernando Goia <f97.goya@gmail.com>
2023-12-19 19:35:22 -0300

    Implementing new transaction dialog style

--- ignite-dt-money/src/components/Header/index.tsx
+++ ignite-dt-money/src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import logoImg from "../../assets/logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import * as Dialog from "@radix-ui/react-dialog";
 
@@ -11,13 +12,7 @@ export function Header() {
           <Dialog.Trigger asChild>
             <NewTransactionButton>Nova transação</NewTransactionButton>
           </Dialog.Trigger>
-          <Dialog.Portal>
-            <Dialog.Overlay />
-            <Dialog.Content>
-              <Dialog.Title>Nova Transação</Dialog.Title>
-              <Dialog.Close />
-            </Dialog.Content>
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
