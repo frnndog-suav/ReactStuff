3772d9aa4e30322bf01effe650420d2dcd72d672
Fernando Goia <f97.goya@gmail.com>
2023-12-19 19:19:26 -0300

    Installing dialog component from radixui lib

--- ignite-dt-money/src/components/Header/index.tsx
+++ ignite-dt-money/src/components/Header/index.tsx
@@ -1,12 +1,24 @@
 import logoImg from "../../assets/logo.svg";
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
