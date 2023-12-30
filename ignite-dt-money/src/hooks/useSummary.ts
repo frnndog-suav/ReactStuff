a355005ea2e0fd77f175470ac32dd30527a180da
Fernando Goia <f97.goya@gmail.com>
2023-12-30 10:28:53 -0300

    Using useMemo

--- ignite-dt-money/src/hooks/useSummary.ts
+++ ignite-dt-money/src/hooks/useSummary.ts
@@ -1,5 +1,6 @@
 import { useContextSelector } from "use-context-selector";
 import { TransactionContext } from "../contexts/TransactionsContext";
+import { useMemo } from "react";
 
 export const useSummary = () => {
   const transactions = useContextSelector(
@@ -7,24 +8,28 @@ export const useSummary = () => {
     (context) => context.transactions
   );
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === "income") {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
+  const summary = useMemo(
+    () =>
+      transactions.reduce(
+        (acc, transaction) => {
+          if (transaction.type === "income") {
+            acc.income += transaction.price;
+            acc.total += transaction.price;
+          } else {
+            acc.outcome += transaction.price;
+            acc.total -= transaction.price;
+          }
 
-      return acc;
-    },
+          return acc;
+        },
 
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    }
+        {
+          income: 0,
+          outcome: 0,
+          total: 0,
+        }
+      ),
+    [transactions]
   );
 
   return summary;
