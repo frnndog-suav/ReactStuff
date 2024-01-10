32668871afb7e615732326ece2fe4ce450c77b69
Fernando Goia <f97.goya@gmail.com>
2024-01-09 21:03:17 -0300

    Implementing dashboard cards

--- ignite-pizza-shop/src/pages/app/Dashboard/index.tsx
+++ ignite-pizza-shop/src/pages/app/Dashboard/index.tsx
@@ -1,10 +1,23 @@
 import { Helmet } from 'react-helmet-async'
 
+import { DailyOrdersAmountCard } from './DailyOrdersAmountCard'
+import { MonthlyCanceledOrdersAmountCard } from './MonthlyCanceledOrdersAmountCard'
+import { MonthlyOrdersAmountCard } from './MonthlyOrdersAmountCard'
+import { MonthlyRevenueCard } from './MonthlyRevenueCard'
+
 export function Dashboard() {
   return (
     <>
       <Helmet title="Dashboard" />
-      <h1>Dashboard</h1>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+        <div className="grid grid-cols-4 gap-4">
+          <MonthlyRevenueCard />
+          <MonthlyOrdersAmountCard />
+          <DailyOrdersAmountCard />
+          <MonthlyCanceledOrdersAmountCard />
+        </div>
+      </div>
     </>
   )
 }
