27f6549e988d7206fc7808d6d6afb0918579fe75
Fernando Goia <f97.goya@gmail.com>
2024-01-09 21:55:35 -0300

    Implementing 404 page

--- ignite-pizza-shop/src/routes/routes.tsx
+++ ignite-pizza-shop/src/routes/routes.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from '@/pages/_layouts/app'
 import { AuthenticationLayout } from '@/pages/_layouts/auth'
 import { Dashboard } from '@/pages/app/Dashboard'
+import { NotFound } from '@/pages/app/Errors/404'
 import { Orders } from '@/pages/app/Orders'
 import { SignIn } from '@/pages/auth/SignIn'
 import { SignUp } from '@/pages/auth/SignUp'
@@ -11,6 +12,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: '/',
